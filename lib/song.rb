e74fcf68144bea05ca2fdce05ee87412c3a0f314
Josh Mulloy <joshmulloy1@gmail.com>
2018-07-09 22:27:23 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -12,7 +12,7 @@ def self.new_by_filename(file_name)
     artist = Artist.find_or_create_by_name(artist_name)
     song.artist = artist
     artist.add_song(song)
-
+    binding.pry
 end
 
 def initialize(name)
