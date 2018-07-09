999d8db2d99fabe19290e91a9528c9bef7c69543
Josh Mulloy <joshmulloy1@gmail.com>
2018-07-09 22:19:41 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -7,10 +7,11 @@ def self.new_by_filename(file_name)
     new_array = file_name.split(" - ")
     name = new_array[1]
     artist_name = new_array[0]
-    
+
     song = Song.new(name)
     artist = Artist.find_or_create_by_name(artist_name)
     song.artist = artist
+    binding.pry
 end
 
 def initialize(name)
