d3c43be8e827b41a4a9b3ab4883c2f0bcf7f01aa
Josh Mulloy <joshmulloy1@gmail.com>
2018-07-09 20:09:06 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -5,8 +5,20 @@ attr_accessor :name, :songs
 @@all = []
 
 def self.all
-  
+
   @@all
+end
+
+def self.find_or_create_by_name(artist_name)
+  @@all.find do |artist|
+    if artist.name == artist_name
+      artist
+    else
+      @@all << self.new(artist_name)
+      
+
+
+
 end
 
   def initialize(name)
@@ -21,6 +33,7 @@ end
   def save
     @@all << self
   end
+
 
 
 
